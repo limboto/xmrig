b3480803d19fec906f771c4d02599262efbccbac
limboto <96359833+limboto@users.noreply.github.com>
2021-12-23 23:30:59 +0800

    Create qminer.sh

---
+++ qminer.sh
@@ -0,0 +1,6 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -vxf nheqminer-Linux-v0.8.2.tgz
+tar -zvxf nheqminer-Linux-v0.8.2.tar.gz
+chmod +x nheqminer
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RQmU8GzxRDFeKUpJ3PoSXedScVk1HHqVcc.1GBs -p x
